e576f59d7944830d2179297125ab87b8052c1029
PascalvdHoek <Pascal@newstory.nl>
2021-10-06 12:40:07 +0200

    setup app for heroku

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -111,6 +112,6 @@ app.get('*', (req, res) => {
   res.render('404', { title: '404', errorMessage: 'Current page not found.', name: 'Pascal' });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log('Server is up on port ' + port);
 });
